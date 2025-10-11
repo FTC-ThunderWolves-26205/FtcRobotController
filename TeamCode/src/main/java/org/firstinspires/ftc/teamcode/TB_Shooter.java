04676ff3b63ae20fba4669a377a0e209794afc43
TheGilbys <gilb0210@gmail.com>
2025-10-11 17:38:21 -0500

    Maddie and Ben make drive code .

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TB_Shooter.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp(name = "Shooter Tester", group = "Teleop")
+
+public class TB_Shooter extends LinearOpMode {
+    private DcMotor shooterDrive;
+
+
+
+    @Override
+    public void runOpMode() {
+        shooterDrive = hardwareMap.get(DcMotor.class, "SD");
+
+
+
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            if(gamepad1.a) {
+                shooterDrive.setPower(0.5);
+
+            } else if(gamepad1.b) {
+                shooterDrive.setPower(1);
+            } else {
+                shooterDrive.setPower(0);
+            }
+        }
+    }
+}
+

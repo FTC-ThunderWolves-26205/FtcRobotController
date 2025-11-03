0978002a429af2a21f6b4443c7f6b284abdc7720
FTC-Thunderwolves <gilb0210@gmail.com>
2025-11-03 14:44:30 -0600

    Disabled MCM_Mecanum and saved Ben's basic shooter tester to TB branch for editing/testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MCM_Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MCM_Mecanum.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Basic Drive", group = "Teleop")
-
+@Disabled
 public class MCM_Mecanum extends LinearOpMode {
     private DcMotor frontRight;
     private DcMotor frontLeft;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TB_Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TB_Shooter.java
@@ -1,36 +1,78 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-@TeleOp(name = "Shooter Tester", group = "Teleop")
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Basic Shooter Testing", group = "Teleop")
 
 public class TB_Shooter extends LinearOpMode {
-    private DcMotor shooterDrive;
-
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    private DcMotor shooter;
+    private boolean prevDpadUp = false;
+    private boolean prevDpadDown = false;
 
 
     @Override
     public void runOpMode() {
-        shooterDrive = hardwareMap.get(DcMotor.class, "SD");
-
-
-
-
+        hardwareStart();
+        double speed = 0.5;
+        double shooterPower = 0;
         waitForStart();
 
         while(opModeIsActive()) {
 
-            if(gamepad1.a) {
-                shooterDrive.setPower(0.5);
+            double forward = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x/2;
 
-            } else if(gamepad1.b) {
-                shooterDrive.setPower(1);
-            } else {
-                shooterDrive.setPower(0);
+            double frontLeftPower = (forward + strafe + turn) * speed;
+            double backLeftPower = (forward - strafe + turn) * speed;
+            double frontRightPower = (forward - strafe - turn) * speed;
+            double backRightPower = (forward + strafe - turn) * speed;
+
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+
+            if (gamepad1.dpad_up && !prevDpadUp) {
+                shooterPower += 0.1;
+            } else if (gamepad1.dpad_down && !prevDpadDown) {
+                shooterPower -= 0.1;
             }
+
+            shooterPower = Math.max(0.0, Math.min(1.0, shooterPower));
+
+            shooter.setPower(shooterPower);
+
+            telemetry.addData("Shooter Power", shooterPower);
+            telemetry.update();
+
+            prevDpadUp = gamepad1.dpad_up;
+            prevDpadDown = gamepad1.dpad_down;
+
         }
+    }
+    private void hardwareStart() {
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        shooter = hardwareMap.get(DcMotor.class, "SD");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
     }
 }
 

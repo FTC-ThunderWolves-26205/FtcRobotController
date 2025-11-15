102c9a50200826385f52ec5bdac4ecc2e0549e6a
ThunderWolves <ThunderWolvesRobotics@gmail.com>
2025-11-15 15:14:16 -0600

    Updating code to Maddie's branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TB_Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TB_Shooter.java
@@ -1,15 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
-// basic shooter testing (incremental) + drive (mechanum) + intake (boolean)
+/*
+This code performs four basic functions:  basic mecanum drive + power-based shooter control + intakes on/off + servo increments.
 
+CONTROLS:
+
+    GAMEPAD 1:
+        LEFT STICK Y: Moves ROBOT forward
+        LEFT STICK X: Strafes ROBOT
+        RIGHT STICK X: Turns ROBOT
+        RIGHT BUMPER: Sets speed to 1 (double speed)
+        LEFT BUMPER: Sets= speed to 0.25 (half speed)
+        LEFT BUMPER & RIGHT BUMPER: Sets= speed to 0.1 (1/5 speed)
+    GAMEPAD 2:
+        DPAD UP: Adds 0.1 (1/10 power) to current shooter power
+        DPAD DOWN: Subtracts 0.1 (1/10 power) to current shooter power
+        DPAD RIGHT: Sets shooter power to 1 (full power) or 0 (no power)
+        A: Sets servo position to 0.5 (resting position) or 0.1 (launching position)
+        LEFT BUMPER: Sets outer intake power to 0 (no power) and 1 (full power)
+        RIGHT BUMPER: Sets inner intake power to 0 (no power) and 1 (full power)
+
+TO DO:  1.  Clean up our edge detection to use FTCLib .wasJustPressed method.  Remove all timers.
+                - driver.wasJustPressed(GamepadKeys.Button.A) is an example
+        2.  Implement .setVelocity and PIDF for shooter.
+        3.  Pick two or three "shooting spots", assign a button on the second controller for each, and code appropriate velocity levels.
+
+LONGER TO DO (Things to Try Before 2nd Tournament?):
+        1.  Add webcam, vision portal, apriltag processor
+        2.  Automate shooting velocity based on detected distance to AprilTag.
+
+Testing showed that -0.2 power appears to work for intake mode on the shooter.
+
+ */
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Basic Shooter Testing", group = "Teleop")
+@TeleOp(name = "Basic TeleOp Mode", group = "Teleop")
 
 public class TB_Shooter extends LinearOpMode {
     private DcMotor frontRight;
@@ -17,20 +51,32 @@ public class TB_Shooter extends LinearOpMode {
     private DcMotor backRight;
     private DcMotor backLeft;
     private DcMotorEx shooter;
-    private DcMotor intake;
-    private boolean wasDpadUp = false;
-    private boolean wasDpadDown = false;
-    private ElapsedTime dpadTimer = new ElapsedTime();
+    private DcMotor iIntake;
+    private DcMotor oIntake;
+    private Servo servo;
 
+
+    private ElapsedTime shooterTimer = new ElapsedTime();
+    private ElapsedTime servoTimer = new ElapsedTime();
+    private ElapsedTime iIntakeTimer = new ElapsedTime();
+    private ElapsedTime oIntakeTimer = new ElapsedTime();
+    private static final double MAX_SERVO = 0.5;
+    private static final double MIN_SERVO = 0.1;
+    private final double TICKS_PER_REV = 28.0; // GoBilda 6k Motor has 28 Ticks per Rev per GoBilda website
 
     @Override
     public void runOpMode() {
         hardwareStart();
         double speed = 0.5;
+        double servoPosition = 0.5;
         double shooterPower = 0;
-        double intakePower = 0;
+        double iIntakePower = 0;
+        double oIntakePower = 0;
         waitForStart();
-        dpadTimer.reset();
+        shooterTimer.reset();
+        servoTimer.reset();
+        iIntakeTimer.reset();
+        oIntakeTimer.reset();
         while(opModeIsActive()) {
 
             double forward = -gamepad1.left_stick_y;
@@ -42,46 +88,58 @@ public class TB_Shooter extends LinearOpMode {
             double frontRightPower = (forward - strafe - turn) * speed;
             double backRightPower = (forward + strafe - turn) * speed;
 
-            frontLeft.setPower(Math.max(0.0, Math.min(1.0, frontLeftPower)));
-            frontRight.setPower(Math.max(0.0, Math.min(1.0, frontRightPower)));
-            backLeft.setPower(Math.max(0.0, Math.min(1.0, backLeftPower)));
-            backRight.setPower(Math.max(0.0, Math.min(1.0, backRightPower)));
+            frontLeft.setPower(clampFull(frontLeftPower));
+            frontRight.setPower(clampFull(frontRightPower));
+            backLeft.setPower(clampFull(backLeftPower));
+            backRight.setPower(clampFull(backRightPower));
+            servo.setPosition(clampServo(servoPosition));
 
-            if (dpadTimer.milliseconds() > 500) {
-                if (gamepad1.dpad_up && !wasDpadUp) {
-                    shooterPower += 0.1;
-                    dpadTimer.reset();
-                } else if (gamepad1.dpad_down && !wasDpadDown) {
-                    shooterPower -= 0.1;
-                    dpadTimer.reset();
-                }
-            }
-
-            if (gamepad1.right_trigger > 0.2) {
+            if (gamepad1.right_bumper) {
                 speed = 1;
-            } else  {
+            } else if(gamepad1.left_bumper) {
+                speed = 0.25;
+            } else if(gamepad1.left_bumper && gamepad1.right_bumper) {
+                speed = 0.1;
+            } else {
                 speed = 0.5;
             }
 
-            if (gamepad1.right_bumper) {
-                intakePower = 1;
-            } else if (gamepad1.left_bumper) {
-                intakePower = 0;
+            if (gamepad2.dpad_up && shooterTimer.milliseconds() > 500) {
+                shooterPower += 0.1;
+                shooterTimer.reset();
+            } else if (gamepad2.dpad_down && shooterTimer.milliseconds() > 500) {
+                shooterPower -= 0.1;
+                shooterTimer.reset();
+            } else if(gamepad2.dpad_right && shooterTimer.milliseconds() > 500) {
+                shooterPower = (shooterPower == 0) ? 1 : 0;
+                shooterTimer.reset();
             }
 
-            shooterPower = Math.max(0.0, Math.min(1.0, shooterPower));
-            intakePower = Math.max(0.0, Math.min(1.0, intakePower));
+            if(gamepad2.a && servoTimer.milliseconds() > 500) {
+                servoPosition = (servoPosition == MAX_SERVO) ? MIN_SERVO : MAX_SERVO;
+                servoTimer.reset();
+            }
 
-            shooter.setPower(shooterPower);
-            intake.setPower(intakePower);
+            if(gamepad2.right_bumper && iIntakeTimer.milliseconds() > 500) {
+                iIntakePower = (iIntakePower == 0) ? 1 : 0;
+                iIntakeTimer.reset();
+            } else if(gamepad2.left_bumper && oIntakeTimer.milliseconds() > 500) {
+                oIntakePower = (oIntakePower == 0) ? 1 : 0;
+                oIntakeTimer.reset();
+            }
+
+            shooter.setPower(clampFull(shooterPower));
+            iIntake.setPower(clampPos(iIntakePower));
+            oIntake.setPower(clampPos(oIntakePower));
 
             telemetry.addData("Shooter Power", shooterPower);
-            telemetry.addData("Sho0ter Velocity",shooter.getVelocity());
-            telemetry.addData("Timer", dpadTimer.milliseconds());
+            telemetry.addData("Shooter Velocity",shooter.getVelocity());
+            telemetry.addData("Servo Position", servoPosition);
+            telemetry.addData("Inner Intake Power", iIntakePower);
+            telemetry.addData("Outer Intake Power", oIntakePower);
+            telemetry.addData("Shooter RPM", ticksPerSecondToRPM(shooter.getVelocity()));
+            telemetry.addData("Battery Voltage", hardwareMap.voltageSensor.iterator().next().getVoltage());
             telemetry.update();
-
-            wasDpadUp = gamepad1.dpad_up;
-            wasDpadDown = gamepad1.dpad_down;
 
         }
     }
@@ -91,16 +149,39 @@ public class TB_Shooter extends LinearOpMode {
         backRight = hardwareMap.get(DcMotor.class, "BR");
         backLeft = hardwareMap.get(DcMotor.class, "BL");
         shooter = hardwareMap.get(DcMotorEx.class, "SD");
-        intake = hardwareMap.get(DcMotor.class,"ID");
+        oIntake = hardwareMap.get(DcMotor.class,"OID");
+        iIntake = hardwareMap.get(DcMotor.class,"IID");
+        servo = hardwareMap.get(Servo.class, "servo");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        oIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        iIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
-        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooter.setDirection(DcMotorSimple.Direction.FORWARD);
+        oIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+        iIntake.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("Status","Initialized");
         telemetry.update();
     }
+
+    private double clampPos(double val) {
+        return Math.max(0.0, Math.min(1.0, val));
+    }
+    private double clampFull(double val) {
+        return Math.max(-1.0, Math.min(1.0, val));
+    }
+    private double clampServo(double val) {
+        return Math.max(MIN_SERVO, Math.min(MAX_SERVO, val));
+    }
+    private double ticksPerSecondToRPM(double tps) { return tps * 60.0 / TICKS_PER_REV; }
+
 }

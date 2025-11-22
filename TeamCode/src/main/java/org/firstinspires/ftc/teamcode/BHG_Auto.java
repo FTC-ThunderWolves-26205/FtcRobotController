7ade15692437c10d4a88586683d24faf784829ea
FTC-Thunderwolves <gilb0210@gmail.com>
2025-11-21 22:52:36 -0600

    Pushing Ben's Auto to TeamBot for Testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BHG_Auto.java
@@ -0,0 +1,134 @@
+/*
+My Autonomous code:
+
+turns on the shooter
+uses servo to kick up the first artifact up to the shooter
+turns on the intakes
+shoots the second artifact
+uses servo to kick up the third artifact up to the shooter
+strafes to the left
+stops everything
+
+ */
+
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "Ben's Auto", group = "Autonomous")
+
+public class BHG_Auto extends LinearOpMode {
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    private DcMotorEx shooter;
+    private DcMotor iIntake;
+    private DcMotor oIntake;
+    private Servo servo;
+
+    private static final double RESTING_SERVO = 0.6;
+    private static final double LAUNCHING_SERVO = 0.1;
+    private final double SHOOTER_SPEED = 0.85;
+    private final long SERVO_DURATION = 750;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        hardwareStart();
+
+        servo.setPosition(RESTING_SERVO);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            shooter.setPower(SHOOTER_SPEED);
+
+            sleep(5000);
+
+            servoMovement();
+
+            intakePowers();
+
+            sleep(4000);
+
+            servoMovement();
+
+            shooter.setPower(0);
+
+            iIntake.setPower(0);
+
+            oIntake.setPower(0);
+
+            setPowers(-1, 1, 1, -1);
+
+            sleep(3000);
+
+            setPowers(0, 0, 0, 0);
+
+            break;
+
+        }
+    }
+    private void hardwareStart() {
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        shooter = hardwareMap.get(DcMotorEx.class, "SD");
+        oIntake = hardwareMap.get(DcMotor.class,"OID");
+        iIntake = hardwareMap.get(DcMotor.class,"IID");
+        servo = hardwareMap.get(Servo.class, "servo");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        oIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        iIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+        oIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+        iIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+    }
+    private void intakePowers() {
+        iIntake.setPower(1);
+        oIntake.setPower(1);
+    }
+    private void servoMovement() {
+        servo.setPosition(LAUNCHING_SERVO);
+
+        sleep(SERVO_DURATION);
+
+        servo.setPosition(RESTING_SERVO);
+    }
+    private void setPowers(double frontLeftPower, double frontRightPower, double
+            backLeftPower, double backRightPower) {
+
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+    }
+}

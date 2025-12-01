0abb8a3268cd4057cdcec18d0e93ab9b26c099c2
FTC-Thunderwolves <gilb0210@gmail.com>
2025-12-01 14:50:10 -0600

    Moving old code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/team/samples/workingcode/JLG_Mecanum.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/team/samples/workingcode/JLG_Mecanum.java
@@ -1,14 +1,15 @@
 //Basic TeleOp Mode for Ben's Bot
 //Controls match main bot
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.robotcontroller.team.samples.workingcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "JLG_Mecanum", group = "Teleop")
-
+@Disabled
 public class JLG_Mecanum extends LinearOpMode{
     public DcMotor frontLeft;
     public DcMotor frontRight;

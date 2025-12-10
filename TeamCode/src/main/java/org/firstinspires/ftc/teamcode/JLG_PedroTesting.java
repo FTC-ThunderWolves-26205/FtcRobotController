d9de7d9bfc5612e63b9d89344a7417fa7c0da012
FTC-Thunderwolves <gilb0210@gmail.com>
2025-12-09 23:56:22 -0600

    Learning pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLG_PedroTesting.java
@@ -0,0 +1,175 @@
+package org.firstinspires.ftc.teamcode.opModes.autonomous;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.arcrobotics.ftclib.controller.PIDFController;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous
+public class JLG_PedroTesting extends LinearOpMode {
+
+    private DcMotorEx shooter;
+    private DcMotor iIntake;
+    private DcMotor oIntake;
+    private Servo servo;
+    private static final double RESTING_SERVO = 0.6;
+    private static final double LAUNCHING_SERVO = 0.1;
+    private final double RANGE = 40;
+    private final long SERVO_DURATION = 500;
+    private PIDFController shooterControl;
+    public static double kP = 0.004;
+    public static double kI = 0.0;
+    public static double kD = 0.00001;
+    public static double kF = 0.00045;
+    private double output;
+    private double targetShooterVelocity = 1460;
+
+    private GoBildaPinpointDriver pinpoint;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        hardwareStart();
+
+        initialize();
+
+        shooterControl = new PIDFController(kP, kI, kD, kF);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+
+        pinpoint.resetPosAndIMU();
+
+        servo.setPosition(RESTING_SERVO);
+
+
+        waitForStart();
+
+        setPathState(0);
+
+
+        while (opModeIsActive()) {
+
+            // We don't need pinpoint.update(); since pedro handles for us
+
+            follower.update();
+            autonomousPathUpdate();
+
+            double shooterVelocity = shooter.getVelocity();
+            output = shooterControl.calculate(shooterVelocity, targetShooterVelocity);
+
+
+
+
+            telemetry.addData("path state", pathState);
+            telemetry.addData("x", follower.getPose().getX());
+            telemetry.addData("y", follower.getPose().getY());
+            telemetry.addData("heading", follower.getPose().getHeading());
+            telemetry.update();
+
+        }
+    }
+    private void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    private Follower follower;
+    private Timer pathTimer;
+    private int pathState;
+
+    // Start Pose
+    private final Pose startPose = new Pose(56, 8, Math.toRadians(90)); // Start position
+
+    // Trajectory Poses
+    private final Pose path1Pose = new Pose(56, 36, Math.toRadians(180)); // Path 1
+    private final Pose path2Pose = new Pose(35.788, 84.514, Math.toRadians(0)); // Path 2
+
+    private PathChain path1Path, path2Path;
+
+    public void buildPaths() {
+        path1Path = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, path1Pose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), path1Pose.getHeading())
+                .build();
+
+        path2Path = follower.pathBuilder()
+                .addPath(new BezierLine(path1Pose, path2Pose))
+                .setLinearHeadingInterpolation(path1Pose.getHeading(), path2Pose.getHeading())
+                .build();
+    }
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(path1Path);
+                setPathState(1);
+                break;
+
+            case 1:
+                if (!follower.isBusy()) {
+                    follower.followPath(path2Path);
+                    setPathState(2);
+                }
+                break;
+
+            case 2:
+                // Done
+                break;
+        }
+    }
+
+    private void initialize() {
+        pathTimer = new Timer();
+        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+        follower.setStartingPose(startPose);
+    }
+
+
+
+    private void update() {
+
+    }
+    private void hardwareStart() {
+
+        shooter = hardwareMap.get(DcMotorEx.class, "SD");
+        oIntake = hardwareMap.get(DcMotor.class,"OID");
+        iIntake = hardwareMap.get(DcMotor.class,"IID");
+        servo = hardwareMap.get(Servo.class, "servo");
+
+        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        oIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        iIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+        oIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+        iIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+    }
+}
+

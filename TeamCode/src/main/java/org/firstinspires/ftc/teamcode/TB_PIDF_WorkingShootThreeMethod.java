947854fd503635cd5b6437d02beaab3f0076a1d4
FTC-Thunderwolves <gilb0210@gmail.com>
2025-12-27 09:28:35 -0600

    12/27/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TB_PIDF_WorkingShootThreeMethod.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TB_PIDF_WorkingShootThreeMethod.java
@@ -24,141 +24,141 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Disabled
 @Autonomous(name = "PIDF Auto Test", group = "Autonomous")
 
-public class                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     TB_PIDF_WorkingShootThreeMethod extends LinearOpMode {
-    private DcMotor frontRight;
-    private DcMotor frontLeft;
-    private DcMotor backRight;
-    private DcMotor backLeft;
-    private DcMotorEx shooter;
-    private DcMotor iIntake;
-    private DcMotor oIntake;
-    private Servo servo;
-    private static final double RESTING_SERVO = 0.6;
-    private static final double LAUNCHING_SERVO = 0.1;
-    private final double RANGE = 40;
-    private final long SERVO_DURATION = 500;
-    private PIDFController shooterControl;
-    public static double kP = 0.004;
-    public static double kI = 0.0;
-    public static double kD = 0.00001;
-    public static double kF = 0.00045;
-    private ElapsedTime timer = new ElapsedTime();
-    private ElapsedTime servoTimer = new ElapsedTime();
-    private double output;
-    private boolean firstTwoShot = false;
-    private boolean thirdShot = false;
-    private double targetShooterVelocity = 1460;
+public class TB_PIDF_WorkingShootThreeMethod extends LinearOpMode {
+
+        private DcMotor frontRight;
+        private DcMotor frontLeft;
+        private DcMotor backRight;
+        private DcMotor backLeft;
+        private DcMotorEx shooter;
+        private DcMotor iIntake;
+        private DcMotor oIntake;
+        private Servo servo;
+        private static final double RESTING_SERVO = 0.6;
+        private static final double LAUNCHING_SERVO = 0.1;
+        private final double RANGE = 40;
+        private final long SERVO_DURATION = 500;
+        private PIDFController shooterControl;
+        public static double kP = 0.004;
+        public static double kI = 0.0;
+        public static double kD = 0.00001;
+        public static double kF = 0.00045;
+        private ElapsedTime timer = new ElapsedTime();
+        private ElapsedTime servoTimer = new ElapsedTime();
+        private double output;
+        private boolean firstTwoShot = false;
+        private boolean thirdShot = false;
+        private double targetShooterVelocity = 1460;
 
 
+        @Override
+        public void runOpMode () throws InterruptedException {
 
+            hardwareStart();
+            boolean firstThree = false;
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+            shooterControl = new PIDFController(kP, kI, kD, kF);
+            FtcDashboard dashboard = FtcDashboard.getInstance();
 
-        hardwareStart();
-        boolean firstThree = false;
+            servo.setPosition(RESTING_SERVO);
 
-        shooterControl = new PIDFController(kP, kI, kD, kF);
-        FtcDashboard dashboard = FtcDashboard.getInstance();
+            waitForStart();
 
-        servo.setPosition(RESTING_SERVO);
+            while (opModeIsActive()) {
 
-        waitForStart();
+                double shooterVelocity = shooter.getVelocity();
+                output = shooterControl.calculate(shooterVelocity, targetShooterVelocity);
 
-        while (opModeIsActive()) {
-
-            double shooterVelocity = shooter.getVelocity();
-            output = shooterControl.calculate(shooterVelocity, targetShooterVelocity);
-
-            TelemetryPacket packet = new TelemetryPacket();
-            packet.put("Target Velocity", targetShooterVelocity);
-            packet.put("Actual Velocity", shooter.getVelocity());
-            packet.put("Output Power", output);
-            dashboard.sendTelemetryPacket(packet);
+                TelemetryPacket packet = new TelemetryPacket();
+                packet.put("Target Velocity", targetShooterVelocity);
+                packet.put("Actual Velocity", shooter.getVelocity());
+                packet.put("Output Power", output);
+                dashboard.sendTelemetryPacket(packet);
 
                 shootThree();
 
-            telemetry.addData("Target Shooter Speed", targetShooterVelocity);
-            telemetry.addData("Shooter Speed", shooter.getVelocity());
+                telemetry.addData("Target Shooter Speed", targetShooterVelocity);
+                telemetry.addData("Shooter Speed", shooter.getVelocity());
+                telemetry.update();
+
+            }
+        }
+        private void hardwareStart () {
+            frontRight = hardwareMap.get(DcMotor.class, "FR");
+            frontLeft = hardwareMap.get(DcMotor.class, "FL");
+            backRight = hardwareMap.get(DcMotor.class, "BR");
+            backLeft = hardwareMap.get(DcMotor.class, "BL");
+            shooter = hardwareMap.get(DcMotorEx.class, "SD");
+            oIntake = hardwareMap.get(DcMotor.class, "OID");
+            iIntake = hardwareMap.get(DcMotor.class, "IID");
+            servo = hardwareMap.get(Servo.class, "servo");
+
+            shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            oIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            iIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+            frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+            oIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+            iIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            telemetry.addData("Status", "Initialized");
             telemetry.update();
-
         }
-    }
-    private void hardwareStart() {
-        frontRight = hardwareMap.get(DcMotor.class, "FR");
-        frontLeft = hardwareMap.get(DcMotor.class, "FL");
-        backRight = hardwareMap.get(DcMotor.class, "BR");
-        backLeft = hardwareMap.get(DcMotor.class, "BL");
-        shooter = hardwareMap.get(DcMotorEx.class, "SD");
-        oIntake = hardwareMap.get(DcMotor.class,"OID");
-        iIntake = hardwareMap.get(DcMotor.class,"IID");
-        servo = hardwareMap.get(Servo.class, "servo");
+        private void intakeSet ( double iIntakePower, double oIntakePower){
+            iIntake.setPower(iIntakePower);
+            oIntake.setPower(oIntakePower);
+        }
+        private void servoMovement () {
+            servo.setPosition(LAUNCHING_SERVO);
+            servoTimer.reset();
+        }
+        private void setPowers ( double frontLeftPower, double frontRightPower, double
+        backLeftPower,double backRightPower){
 
-        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        oIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        iIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
-        oIntake.setDirection(DcMotorSimple.Direction.FORWARD);
-        iIntake.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        telemetry.addData("Status","Initialized");
-        telemetry.update();
-    }
-    private void intakeSet(double iIntakePower, double oIntakePower) {
-        iIntake.setPower(iIntakePower);
-        oIntake.setPower(oIntakePower);
-    }
-    private void servoMovement() {
-        servo.setPosition(LAUNCHING_SERVO);
-        servoTimer.reset();
-    }
-    private void setPowers(double frontLeftPower, double frontRightPower, double
-            backLeftPower, double backRightPower) {
-
-        frontLeft.setPower(frontLeftPower);
-        frontRight.setPower(frontRightPower);
-        backLeft.setPower(backLeftPower);
-        backRight.setPower(backRightPower);
-    }
-
-    private void shootThree() {
-        shooter.setPower(Math.abs(output));
-
-        if(!firstTwoShot && timer.milliseconds() == 0) {
-            timer.reset();
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
         }
 
-        if (atTargetSpeed(shooter.getVelocity(), targetShooterVelocity ,RANGE) && !firstTwoShot && timer.milliseconds() > 1000) {
-            intakeSet(1,1);
-            timer.reset();
-            firstTwoShot = true;
-        }
-        if (firstTwoShot && !thirdShot && timer.milliseconds() > 2000) {
-            servoMovement();
-            thirdShot = true;
-        }
+        private void shootThree () {
+            shooter.setPower(Math.abs(output));
+
+            if (!firstTwoShot && timer.milliseconds() == 0) {
+                timer.reset();
+            }
+
+            if (atTargetSpeed(shooter.getVelocity(), targetShooterVelocity, RANGE) && !firstTwoShot && timer.milliseconds() > 1000) {
+                intakeSet(1, 1);
+                timer.reset();
+                firstTwoShot = true;
+            }
+            if (firstTwoShot && !thirdShot && timer.milliseconds() > 2000) {
+                servoMovement();
+                thirdShot = true;
+            }
 
 
-        if (thirdShot && servoTimer.milliseconds() > SERVO_DURATION) {
-            servo.setPosition(RESTING_SERVO);
-            intakeSet(0,0);
-            sleep(400);
-            requestOpModeStop();
+            if (thirdShot && servoTimer.milliseconds() > SERVO_DURATION) {
+                servo.setPosition(RESTING_SERVO);
+                intakeSet(0, 0);
+                sleep(400);
+                requestOpModeStop();
+            }
+        }
+
+        public boolean atTargetSpeed ( double shooterVelocity, double targetVelocity, double range){
+            return Math.abs(shooterVelocity - targetVelocity) <= range;
         }
     }
 
-    public boolean atTargetSpeed(double shooterVelocity, double targetVelocity, double range) {
-        return Math.abs(shooterVelocity - targetVelocity) <= range;
-    }
-}

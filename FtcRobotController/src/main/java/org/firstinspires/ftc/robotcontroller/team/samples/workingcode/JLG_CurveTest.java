206a0c373f6c9b3867ca8c886f4f59982603167f
FTC-Thunderwolves <gilb0210@gmail.com>
2025-12-31 08:09:33 -0600

    12/31/2025

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/team/samples/workingcode/JLG_CurveTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/team/samples/workingcode/JLG_CurveTest.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+//import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 @Disabled
 @Autonomous(name = "JLG Curve Test", group = "Autonomous")
 public class JLG_CurveTest extends LinearOpMode {

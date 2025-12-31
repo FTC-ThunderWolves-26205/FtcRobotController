f838ef28e1f5ea51d314569e123ffabc35828be1
FTC-Thunderwolves <gilb0210@gmail.com>
2025-12-30 21:59:20 -0600

    Base code for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLG_PedroTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLG_PedroTesting.java
@@ -1,5 +1,6 @@
-//In theory, back up, shoot 3, intake first set.  stop
-//start bot centered on the line for blue goal
+//Use this base code to build auto.  Use visualizer to get pose points and build paths.
+//See comments below.
+//DO NOT TOUCH SHOOT THREE METHOD.
 
 
 package org.firstinspires.ftc.teamcode;

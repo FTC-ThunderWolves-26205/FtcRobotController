7f4ec373f83387139623e38ede8c1184aeecc6ac
FTC-Thunderwolves <gilb0210@gmail.com>
2025-12-30 22:00:32 -0600

    Code for Maddie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MCM_PedroAuto.java
@@ -0,0 +1,304 @@
+//Use this base code to build auto.  Use visualizer to get pose points and build paths.
+//See comments below.
+//DO NOT TOUCH SHOOT THREE METHOD.
+
+
+package org.firstinspires.ftc.teamcode;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous(name = "JLG Close Blue Test", group = "Autonomous")
+
+public class MCM_PedroAuto extends LinearOpMode {
+
+    private DcMotorEx shooter;
+    private DcMotor iIntake;
+    private DcMotor oIntake;
+    private Servo servo;
+    private static final double RESTING_SERVO = 0.35;
+    private static final double LAUNCHING_SERVO = 0.;
+    private final double RANGE = 40;
+    private final long SERVO_DURATION = 500;
+    private PIDFController shooterControl;
+    public static double kP = 0.004;
+    public static double kI = 0.0;
+    public static double kD = 0.00001;
+    public static double kF = 0.00045;
+    private double output;
+    private double targetShooterVelocity = 1460;
+
+    private GoBildaPinpointDriver pinpoint;
+
+    private ElapsedTime timer = new ElapsedTime();
+    private ElapsedTime servoTimer = new ElapsedTime();
+
+    private Follower follower;
+
+
+    /*
+    POSES GO HERE.
+    ADD A COMMENT AFTER EACH POSE DESCRIBING WHAT IT IS.
+     */
+    private final Pose startPose = new Pose(25, 119, Math.toRadians(145)); // Start position
+    private final Pose firstShotPose = new Pose(57, 86, Math.toRadians(145)); // Pose for First Shot
+    private final Pose intakeFirst = new Pose(19, 86, Math.toRadians(180)); // Pose for Intake 3 more
+
+    //PATHS GO HERE.  USE DESCRIPTIVE NAMES.
+    private PathChain firstShotPath, firstIntakePath;
+
+
+
+    //ENUM DEFINING STATES FOR AUTO PATH.  YOU MUST HAVE A WAIT STEP AFTER ANY STEP THAT MOVES THE ROBOT.
+    //THE WAIT STEP MUST INCLUDE A CHECK TO SEE IF FOLLOWER.ISBUSY IS FALSE
+    private enum AutoState {
+        START_PATH1,
+        WAIT_PATH1,
+        SHOOT,
+        WAIT_PATH2,
+        END
+    }
+    //ENUM DEFINING STATES FOR SHOOTER.  DON'T MODIFY THIS AS IT TIES TO OUR SHOOTTHREE METHOD.
+    private enum ShooterState {
+        IDLE,
+        SHOOT_TWO,
+        SHOOT_THIRD,
+        STOP_INTAKES,
+        END
+    }
+
+    //SETTING STATES FOR OUR TWO FSM'S
+    private AutoState autoState = AutoState.START_PATH1;
+    private ShooterState shooterState = ShooterState.IDLE;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        hardwareStart();  //Initializing all our non-movement hardware.
+
+        initialize();  //Initializing Pedro, Building Paths
+
+        shooterControl = new PIDFController(kP, kI, kD, kF);
+        FtcDashboard dashboard = FtcDashboard.getInstance();  //not using dashboard but keep for now in case we do
+
+        servo.setPosition(RESTING_SERVO);
+
+
+        waitForStart();
+
+
+
+
+        while (opModeIsActive()) {
+
+            // We don't need pinpoint.update(); since pedro handles for us
+
+            follower.update();
+
+
+
+
+            autonomousPathUpdate(); // This calls our state machine.  It's all we need in the main loop
+            //Ben wants to put the state machine here instead and just get rid of autonomousPathUpdate().  Considering it...
+
+
+
+
+
+
+
+            telemetry.addData("x", follower.getPose().getX());
+            telemetry.addData("y", follower.getPose().getY());
+            telemetry.addData("heading", follower.getPose().getHeading());
+            telemetry.update();
+
+        }
+    }
+
+
+
+    //BUILD PATHS HERE
+    //EACH MUST BE INTRODUCED ABOVE IN THE PATHCHAIN FIRST
+    //USE DESCRIPTIVE NAME, ACTION OR DESTINATION
+    public void buildPaths() {
+        firstShotPath = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, firstShotPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), firstShotPose.getHeading())
+                .build();
+
+        firstIntakePath = follower.pathBuilder()
+                .addPath(new BezierLine(firstShotPose, intakeFirst))
+                .setLinearHeadingInterpolation(firstShotPose.getHeading(), intakeFirst.getHeading())
+                .build();
+    }
+
+    /*
+    Here is our main FSM for the auto.
+    We MUST add a wait after ANY movement.  The wait should include a check to see if the follower.isBusy() is false.
+    Leave comment describing what you are doing on each path.
+     */
+    public void autonomousPathUpdate() {
+        switch (autoState) {
+
+            case START_PATH1:  //Back up from starting position.
+                follower.followPath(firstShotPath);
+                autoState = AutoState.WAIT_PATH1;
+                break;
+
+            case WAIT_PATH1:  //Wait after movement.
+                if (!follower.isBusy()) {
+                    shooterState = ShooterState.IDLE;   // reset shooter FSM
+                    autoState = AutoState.SHOOT;
+                }
+                break;
+
+            case SHOOT:  //Shoot Three, then turn on intakes and move to first set to intake.
+                shootThree();
+
+                if (shooterState == ShooterState.END) {
+                    intakeSet(0.5, 0.5);
+                    follower.followPath(firstIntakePath);
+                    shooterState = ShooterState.IDLE; //Resetting shooter state for the next shootThree()
+                    autoState = AutoState.WAIT_PATH2;
+                }
+                break;
+
+
+            case WAIT_PATH2:  //Wait after movement.  Stop intakes.
+                if (!follower.isBusy()) {
+                    intakeSet(0, 0);
+                    autoState = AutoState.END;
+                }
+                break;
+
+            case END: //Always have an END.  Seems to be recommended to keep it empty.
+
+                break;
+        }
+    }
+
+    /*
+    Here is our FSM for shooting three.
+     */
+    private void shootThree() {
+        double shooterVelocity = shooter.getVelocity();
+        output = shooterControl.calculate(shooterVelocity, targetShooterVelocity);
+
+        if (targetShooterVelocity > 0) {
+            shooter.setPower(output);
+        } else {
+            shooter.setPower(0);
+        }
+
+
+
+        switch (shooterState) {
+
+            case IDLE:
+                timer.reset();
+                servoTimer.reset();
+                targetShooterVelocity = 1460;
+                shooterState = ShooterState.SHOOT_TWO;
+                break;
+
+
+            case SHOOT_TWO:
+
+                if (atTargetSpeed(shooter.getVelocity(), targetShooterVelocity, RANGE) && timer.milliseconds() > 1000) {
+                    intakeSet(1, 1);
+                    timer.reset();
+                    shooterState = ShooterState.SHOOT_THIRD;
+                }
+                break;
+
+            case SHOOT_THIRD:
+                if (timer.milliseconds() > 2000) {
+                    servoMovement();
+                    shooterState = ShooterState.STOP_INTAKES;
+                }
+                break;
+
+            case STOP_INTAKES:
+                if (servoTimer.milliseconds() > SERVO_DURATION) {
+                    servo.setPosition(RESTING_SERVO);
+                    intakeSet(0, 0);
+                    targetShooterVelocity = 0;
+                    shooterState = ShooterState.END;
+                }
+                break;
+
+            case END:
+
+                break;
+        }
+    }
+
+    private void initialize() {
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
+
+    }
+
+
+
+
+    private void hardwareStart() {
+
+        shooter = hardwareMap.get(DcMotorEx.class, "SD");
+        oIntake = hardwareMap.get(DcMotor.class,"OID");
+        iIntake = hardwareMap.get(DcMotor.class,"IID");
+        servo = hardwareMap.get(Servo.class, "servo");
+
+        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        oIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        iIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+        oIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+        iIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+    }
+
+    private void intakeSet(double iIntakePower, double oIntakePower) {
+        iIntake.setPower(iIntakePower);
+        oIntake.setPower(oIntakePower);
+    }
+    private void servoMovement() {
+        servo.setPosition(LAUNCHING_SERVO);
+        servoTimer.reset();
+    }
+
+    public boolean atTargetSpeed(double shooterVelocity, double targetVelocity, double range) {
+        return Math.abs(shooterVelocity - targetVelocity) <= range;
+    }
+}
+

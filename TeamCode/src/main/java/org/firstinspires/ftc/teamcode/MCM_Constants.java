b4a9bc36133bb138edfd4a0cf46bf0efa6df701d
Beaucage66 <chrisbeaucage@gmail.com>
2026-01-01 11:59:02 -0600

    :|

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MCM_Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MCM_Constants.java
@@ -1,9 +1,24 @@
 package org.firstinspires.ftc.teamcode;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class MCM_Constants {
     static double resting_servo = 0.35;
     static double launching_servo = 0.05;
 
-    pose FarStart = new pose
+    Pose FarStart = new Pose(48,9,90);
 
 }

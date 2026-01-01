5de0c9667f76a0be2715816e26eaba794336d403
Beaucage66 <chrisbeaucage@gmail.com>
2026-01-01 11:46:23 -0600

    :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MCM_Constants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public class MCM_Constants {
+    static double resting_servo = 0.35;
+    static double launching_servo = 0.05;
+
+    pose FarStart = new pose
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MCM_FarBlueAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Far Blue Auto",group = "Autonomus")
+public class MCM_FarBlueAuto {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
